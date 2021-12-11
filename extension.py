f54d832f975fc07393b7cc3d1c0b138cbdd7c72f
abhishek <ahirraoabhishek007@gmail.com>
2021-12-11 16:48:27 +0530

    changed extension
    Due to failed automation process all the files had a .java extension changed that to appropiate .cpp extension.

---
+++ extension.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+directory = os.path.dirname(os.path.realpath(sys.argv[0])) #get the directory of your script
+for subdir, dirs, files in os.walk(directory):
+ for filename in files:
+  if filename.find('.java') > 0:
+   subdirectoryPath = os.path.relpath(subdir, directory) #get the path to your subdirectory
+   filePath = os.path.join(subdirectoryPath, filename) #get the path to your file
+   newFilePath = filePath.replace(".java",".cpp") #create the new name
+   os.rename(filePath, newFilePath) 
